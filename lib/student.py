30d80fdfb3542ae5998f8b244127b8d960628e82
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-25 11:52:24 -0400

    pass tests

--- lib/student.py
+++ lib/student.py
@@ -4,7 +4,8 @@ from user import User
 
 class Student(User):
     
-    def __init__(self, knowledge):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
         self.knowledge = []
     
     def learn(self, string):
--- lib/teacher.py
+++ lib/teacher.py
@@ -4,21 +4,21 @@ from user import User
 
 import random
 
-knowledge = [
-    "str is a data type in Python",
-    "programming is hard, but it's worth it",
-    "JavaScript async web request",
-    "Python function call definition",
-    "object-oriented teacher instance",
-    "programming computers hacking learning terminal",
-    "pipenv install pipenv shell",
-    "pytest -x flag to fail fast",
-]
 
 class Teacher(User):
     
-    def __init__(self, knowledge):
-        self.knowledge = knowledge
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+            ]
 
     def teach(self):
         return random.choice(self.knowledge)
